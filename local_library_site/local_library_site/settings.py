9838ef6df3ce3db4d5165b85e84516ce09fcaaed
JBob47 <justin@justinbobbittdesign.com>
2018-11-08 11:29:16 -0500

    Part 10: Testing, up to Challenge

--- local_library_site/local_library_site/settings.py
+++ local_library_site/local_library_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
